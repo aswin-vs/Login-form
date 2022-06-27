60daee9be75ecdb5fc0885c94547f7ad0c28c3e7
aswin-vs <aswingenius@gmail.com>
2022-06-27 16:56:20 +0530

    greeting update

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const myDate = new Date();
+const hrs = myDate.getHours();
+var greet;
+
+if (hrs < 12) {
+	greet = 'Good Morning';
+}
+else if (hrs >= 12 && hrs < 17) {
+	greet = 'Good Afternoon';
+}
+else if (hrs >= 17 && hrs <= 24) {
+  greet = 'Good Evening';
+}
+
+document.getElementById('greet2').innerHTML = greet;
+
 const pass_field = document.querySelector(".pass-key");
 const showBtn = document.querySelector(".show");
 
